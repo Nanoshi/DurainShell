6f4e6391426003a12a435c77fc52feec4643193f
Nanoshi <42802559+Nanoshi@users.noreply.github.com>
2018-09-14 08:35:50 +0200

    Create menu.ps1

---
+++ menu.ps1
@@ -0,0 +1,10 @@
+<#
+Still deciding on a menu structure
+
+Might use Out-menu: https://github.com/gangstanthony/PowerShell/blob/master/Out-Menu.ps1
+or out-gridview -passthrough. Probably going for the latter.
+
+Input: PSObject (any fields + return)
+Output: Index of object chosen? 
+
+#>
