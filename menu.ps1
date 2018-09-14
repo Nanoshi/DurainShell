7237bb446e47ed1dbf80b78f2c4386a2e61c56ca
Nanoshi <42802559+Nanoshi@users.noreply.github.com>
2018-09-14 10:29:20 +0200

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -7,4 +7,23 @@ or out-gridview -passthrough. Probably going for the latter.
 Input: PSObject (any fields + return)
 Output: Index of object chosen? 
 
+Combine the 2 options! Use meun for fast movement and Gridview for complex choices
+
+
+    Title
+Name                 Paramater    Group ID
+
+<back/cancel> Go back to previous screen
+<save/commit> Save settings
+Current command: ma
+Current param: -PS 443,80
+
+
+Group name            <required>  
+(x) Radio 1           <required>  Gr1-R1 
+( ) Radio 2           <optional>
+Toggle
+[x] Enable x          <N/A>
+
+
 #>
