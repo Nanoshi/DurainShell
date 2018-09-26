22c092a2b4de3b80922096d6110171520d6ddabf
Nanoshi <lorengarth@gmail.com>
2018-09-26 22:08:01 +0200

    beautify and restructure

--- durianshell.ps1
+++ durianshell.ps1
@@ -11,11 +11,11 @@ $sleepTimer = 1
 # Set PWD to script location
 Set-Location (Split-Path $script:MyInvocation.MyCommand.Path)
 $tools = New-Object psobject
-$import = Get-Content -path "./tools/nmap.json" -raw | ConvertFrom-Json
+$import = Get-Content -Path "./tools/nmap.json" -Raw | ConvertFrom-Json
 # Import tools, then overlay with user settings
 
 # If the tool is installed, add it to the list
-$tools | Add-Member -Name $import.name -Value $import -MemberType NoteProperty
+$tools | Add-Member -Name $import.Name -Value $import -MemberType NoteProperty
 
 # Set up the initial navigation #
 $scan = $null
@@ -33,197 +33,198 @@ $navigation | Add-Member -MemberType NoteProperty -Name option -Value ""
 # Loop forever
 while (1) {
 
-#region Menu Header
-Clear-Host
-# Clear the previous choices
-$menuChoices = New-Object psobject
-$counter = 1
+	#region Menu Header
+	Clear-Host
+	# Clear the previous choices
+	$menuChoices = New-Object psobject
+	$counter = 1
 
-# Header Bar
-Write-Host
-if ($navigation.location) { Write-Host -NoNewline "$($navigation.location)"}
-if ($navigation.tool) { Write-Host -NoNewline " > $($navigation.tool)"}
-if ($navigation.param) { Write-Host -NoNewline " > $($navigation.option)"}
-Write-Host "`n"
-Write-Host "Jobs running: NMAP 1, Hydra 2"
-Write-Host
-#endregion
+	# Header Bar
+	Write-Host
+	if ($navigation.location) { Write-Host -NoNewline "$($navigation.location)" }
+	if ($navigation.tool) { Write-Host -NoNewline " > $($navigation.tool)" }
+	if ($navigation.param) { Write-Host -NoNewline " > $($navigation.option)" }
+	Write-Host "`n"
+	Write-Host "Jobs running: NMAP 1, Hydra 2"
+	Write-Host
+	#endregion
 
-#region Menu Home
+	#region Menu Home
 
-#endregion
+	#endregion
 
-#############
-# Tool Menu #
-#############
+	#############
+	# Tool Menu #
+	#############
 
-#region Menu - Tool
-if ($navigation.location -like "Tools") {
-    # Menu - Tool selection
-    if ($navigation.tool -like ""){
+	#region Menu - Tool
+	if ($navigation.location -like "Tools") {
+		# Menu - Tool selection
+		if ($navigation.tool -like "") {
 
-    }
-    # Menu - Tool - Option selection
-    if ($navigation.tool -notlike "") {
-    # Configure $scan from tool if empty
-        if ($null -eq $scan){
-        $scan = New-Object psobject
-        $scan | Add-Member -MemberType NoteProperty -Name "tool" -Value $navigation.tool
-        # Iterate through each Tool Option
-        $tools.($navigation.tool).options.PSObject.Properties | ForEach-Object {
-            $entry = New-Object PSObject
-            $entry |Add-Member -MemberType NoteProperty -Name "enabled" -Value ($_.value.enabled -eq $true)
-            if ($tools.($navigation.tool).options.($_.name).param){
-                $entry | Add-Member -MemberType NoteProperty -Name "param" -Value $tools.($navigation.tool).options.$($_.name).param.defualts[0]
-            }else{
-                $entry | Add-Member -MemberType NoteProperty -Name "param" -Value ""
-            }
-            $scan | Add-Member -MemberType NoteProperty -Name $_.name -Value $entry
-        } # End For-Each loop 
-    } # End if
-    } # End Tool - Option
-} # End tool
-    # Logic about selected targets
-    Write-Host "Target selected: None"
+		}
+		# Menu - Tool - Option selection
+		if ($navigation.tool -notlike "") {
+			# Configure $scan from tool if empty
+			if ($null -eq $scan) {
+				$scan = New-Object psobject
+				$scan | Add-Member -MemberType NoteProperty -Name "tool" -Value $navigation.tool
+				# Iterate through each Tool Option
+				$tools.($navigation.tool).options.PSObject.Properties | ForEach-Object {
+					$entry = New-Object PSObject
+					$entry | Add-Member -MemberType NoteProperty -Name "enabled" -Value ($_.value.enabled -eq $true)
+					if ($tools.($navigation.tool).options.($_.Name).param) {
+						$entry | Add-Member -MemberType NoteProperty -Name "param" -Value $tools.($navigation.tool).options.$($_.Name).param.defualts[0]
+					} else {
+						$entry | Add-Member -MemberType NoteProperty -Name "param" -Value ""
+					}
+					$scan | Add-Member -MemberType NoteProperty -Name $_.Name -Value $entry
+				} # End For-Each loop 
+			} # End if blank scan
 
-    # Command Builder
-    Write-Host "Command: $($tools.($scan.tool).command)" -NoNewline #Notice the trailing space
+			# Logic about selected targets
+			Write-Host "Target selected: None"
 
-    # Replace tool.output.param with scan.param
-    $tools.($scan.tool).options.PSObject.Properties | Where-Object {
-        $scan.($_.name).enabled -eq $true } | ForEach-Object {
-        Write-Host -NoNewline " $($tools.$($scan.tool).options.($_.name).output.Replace("<param>","$($scan.($_.name).param)"))" 
+			# Command Builder
+			Write-Host "Command: $($tools.($scan.tool).command)" -NoNewline #Notice the trailing space
 
-    } # End For-Each loop 
+			# Replace tool.output.param with scan.param
+			$tools.($scan.tool).options.PSObject.Properties | Where-Object {
+				$scan.($_.Name).enabled -eq $true } | ForEach-Object {
+				Write-Host -NoNewline " $($tools.$($scan.tool).options.($_.name).output.Replace("<param>","$($scan.($_.name).param)"))"
+
+			} # End For-Each loop 
 
 
-    Write-Host "`n`nWhich do you want to toggle?" 
-    Write-Host "`n<enter> Cancel/Back"
+			Write-Host "`n`nWhich do you want to toggle?"
+			Write-Host "`n<enter> Cancel/Back"
 
-    # Iterate through each Tool-Option while keeping the order
-    $tools.($navigation.tool).options.PSobject.Properties | ForEach-Object {
+			# Iterate through each Tool-Option while keeping the order
+			$tools.($navigation.tool).options.PSObject.Properties | ForEach-Object {
 
-        if ($scan.($_.name).enabled){$enabled = "X"}
-        else {$enabled = " "}
-        Write-Host -NoNewline $counter
-        if ($_.value.group){
-            Write-Host -NoNewline " ($enabled) "  
-        }
-        else {
-            Write-Host -NoNewline " [$enabled] "  
-        }
+				if ($scan.($_.Name).enabled) { $enabled = "X" }
+				else { $enabled = " " }
+				Write-Host -NoNewline $counter
+				if ($_.value.group) {
+					Write-Host -NoNewline " ($enabled) "
+				}
+				else {
+					Write-Host -NoNewline " [$enabled] "
+				}
 
-        Write-Host $_.value.description
+				Write-Host $_.value.description
 
-        # Save list and increment
-        $menuChoices | Add-Member -MemberType NoteProperty -Name $counter -Value $_.Name
-        $counter++
+				# Save list and increment
+				$menuChoices | Add-Member -MemberType NoteProperty -Name $counter -Value $_.Name
+				$counter++
 
-        # Display the extra paramater, if any
-        if ($_.value.param.required){
-            if($scan.($_.name)){
-                Write-host "$counter     < $($scan.($_.name).param) >"}
-            else{
-                Write-Host "$counter     < Mandatory >"}
-        # For param options, saves menu option as an array
-        $menuChoices | Add-Member -MemberType NoteProperty -Name $counter -Value ($_.name,"param")
-        $counter++
+				# Display the extra paramater, if any
+				if ($_.value.param.required) {
+					if ($scan.($_.Name)) {
+						Write-Host "$counter     < $($scan.($_.name).param) >" }
+					else {
+						Write-Host "$counter     < Mandatory >" }
+					# For param options, saves menu option as an array
+					$menuChoices | Add-Member -MemberType NoteProperty -Name $counter -Value ($_.Name,"param")
+					$counter++
 
-        }
-    } # End of For-Each Option
+				}
+			} # End of For-Each Option
 
-    # Add default settings, scan and exit
-    # Catch these outside of the MenuChoice
-    Write-host
-    Write-Host "d     Restore default settings"
-    Write-Host "s     Save current settings"
-    Write-Host "e     Exit Program"
-    Write-Host "r     Run the scan"
-    Write-Host
-} # End tool menu
-#endregion
+			# Add default settings, scan and exit
+			# Catch these outside of the MenuChoice
+			Write-Host
+			Write-Host "d     Restore default settings"
+			Write-Host "s     Save current settings"
+			Write-Host "e     Exit Program"
+			Write-Host "r     Run the scan"
+			Write-Host
 
-#region Menu - Tool - Option - Param
-if ($navigation.location -like "Tools" -and $navigation.tool -notlike "" -and $navigation.option -notlike ""){
-    Write-Host "Which paramater would you like to use?`n"
-    Write-Host "Current: $($scan.($navigation.option).param)`n"
-    
-} # End Param choice  
-#endregion
+		} # End Tool - Option
+	} # End tool
+	#endregion
 
-##################
-# Receive Inputs #
-##################
+	#region Menu - Tool - Option - Param
+	if ($navigation.location -like "Tools" -and $navigation.tool -notlike "" -and $navigation.option -notlike "") {
+		Write-Host "Which paramater would you like to use?`n"
+		Write-Host "Current: $($scan.($navigation.option).param)`n"
 
-# Recieve and validate input
-[string]$choice = $null
-[string]$choice = Read-Host -Prompt "Pick a number " -ErrorAction SilentlyContinue
+	} # End Param choice  
+	#endregion
 
-# If back/cancel then clear scan variable
-if ($choice -like ""){
-    Write-host "Going up 1 menu"; Start-Sleep -Seconds $sleepTimer; Continue;
-}
+	##################
+	# Receive Inputs #
+	##################
 
-if ($choice -match "e"){
-    Write-host "Exiting program"; Start-Sleep -Seconds $sleepTimer; break;
-}
+	# Recieve and validate input
+	[string]$choice = $null
+	[string]$choice = Read-Host -Prompt "Pick a number " -ErrorAction SilentlyContinue
 
-if ($choice -match "d"){
-# Go back 1 menu
-    Write-host "Default settings!";
-    $scan = $null
-    Start-Sleep -Seconds $sleepTimer; Continue;
-}
+	# If back/cancel then clear scan variable
+	if ($choice -like "") {
+		Write-Host "Going up 1 menu"; Start-Sleep -Seconds $sleepTimer; continue;
+	}
 
-if ($choice -notmatch '^[0-9]+$'){
-    Write-host "Please type a number"; Start-Sleep -Seconds $sleepTimer; Continue;
-}
+	if ($choice -match "e") {
+		Write-Host "Exiting program"; Start-Sleep -Seconds $sleepTimer; break;
+	}
 
-if ([int]$choice -lt 1 -or [int]$choice -ge $counter){
-    Write-Host "Please choose a number in the range above"; Start-Sleep -Seconds $sleepTimer; Continue;
-}
+	if ($choice -match "d") {
+		# Go back 1 menu
+		Write-Host "Default settings!";
+		$scan = $null
+		Start-Sleep -Seconds $sleepTimer; continue;
+	}
 
-# Toggle variables + export settings
-# Ideas: Filter Navigation first then options ***
+	if ($choice -notmatch '^[0-9]+$') {
+		Write-Host "Please type a number"; Start-Sleep -Seconds $sleepTimer; continue;
+	}
 
-#region Input - Tools - Options
-if ($navigation.location -like "Tools" -and $navigation.tool -notlike "" -and $navigation.option -like ""){
-    # Simple 1d choice
-    if ($menuChoices.($choice).count -eq 1){
-        Write-Host "1 Param"
-        # Cut out if EXIT
-        if ($menuChoices.($choice) -match "exit") { Continue; }
-        
-        # Save options and execute command
-        if ($menuChoices.($choice) -match "run") { Continue; }
+	if ([int]$choice -lt 1 -or [int]$choice -ge $counter) {
+		Write-Host "Please choose a number in the range above"; Start-Sleep -Seconds $sleepTimer; continue;
+	}
 
-        # Toggle options
-        if ($navigation.location -like "Tools" -and $navigation.tool -notlike "") {
-            Write-Host "Toggle"
-            $scan.($menuChoices.($choice)).enabled = !($scan.($menuChoices.($choice)).enabled)
-            # If option grouping is not blank
-            $toggleGroup = $tools.($navigation.tool).options.($menuChoices.($choice)).group
-            if ($toggleGroup -notlike "" -and $scan.($menuChoices.($choice)).enabled){
-                # Loops through tool-options, then turn off others from group
-                $tools.($scan.tool).options.PSObject.Properties | Where-Object {
-                $tools.($scan.tool).options.($_.name).group -eq $toggleGroup -and
-                $_.name -notlike $menuChoices.$choice} | ForEach-Object {
-                    $scan.($_.name).enabled = $false
-                } # End Foreach-Ob Group
-            } # End if group
-        }  # End toggle if
-        Continue # Back to the top of the menu
-    } # End Choice 1
+	# Toggle variables + export settings
+	# Ideas: Filter Navigation first then options ***
 
-    # Paramater handling
-    if ($menuChoices.($choice).count -eq 2){
-        $navigation.option = $menuChoices.($choice)
-        Continue 
-    } # End Param
-} # End Input Tool Options
-#endregion
+	#region Input - Tools - Options
+	if ($navigation.location -like "Tools" -and $navigation.tool -notlike "" -and $navigation.option -like "") {
+		# Simple 1d choice
+		if ($menuChoices.($choice).count -eq 1) {
+			Write-Host "1 Param"
+			# Cut out if EXIT
+			if ($menuChoices.($choice) -match "exit") { continue; }
 
-Write-Host "Input didn't match with anything, something went wrong..."; Start-Sleep; 
+			# Save options and execute command
+			if ($menuChoices.($choice) -match "run") { continue; }
+
+			# Toggle options
+			if ($navigation.location -like "Tools" -and $navigation.tool -notlike "") {
+				Write-Host "Toggle"
+				$scan.($menuChoices.($choice)).enabled = !($scan.($menuChoices.($choice)).enabled)
+				# If option grouping is not blank
+				$toggleGroup = $tools.($navigation.tool).options.($menuChoices.($choice)).group
+				if ($toggleGroup -notlike "" -and $scan.($menuChoices.($choice)).enabled) {
+					# Loops through tool-options, then turn off others from group
+					$tools.($scan.tool).options.PSObject.Properties | Where-Object {
+						$tools.($scan.tool).options.($_.Name).group -eq $toggleGroup -and
+						$_.Name -notlike $menuChoices.$choice } | ForEach-Object {
+						$scan.($_.Name).enabled = $false
+					} # End Foreach-Ob Group
+				} # End if group
+			} # End toggle if
+			continue # Back to the top of the menu
+		} # End Choice 1
+
+		# Paramater handling
+		if ($menuChoices.($choice).count -eq 2) {
+			$navigation.option = $menuChoices.($choice)
+			continue
+		} # End Param
+	} # End Input Tool Options
+	#endregion
+
+	Write-Host "Input didn't match with anything, something went wrong..."; Start-Sleep;
 } # End For ever loop, back to the top.
 
 #########
