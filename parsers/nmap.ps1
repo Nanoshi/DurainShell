6f3c036c26c1c516544d404c077dfa48d587fe67
Nanoshi <lorengarth@gmail.com>
2018-10-11 22:09:36 +0200

    worked on live host parser

--- parsers/nmap.ps1
+++ parsers/nmap.ps1
@@ -1,10 +1,25 @@
-﻿
+﻿[xml]$nmap = Get-Content .\nmap.xml
 
+#Strip out the offline titles
 $liveHosts = $nmap.nmaprun.host | ? { $_.status.state -match "up"}
 
-$liveHosts[0].os.osmatch
+$targets = @()
 
-$liveHosts[0].os.osmatch.accuracy
-$liveHosts[0].os.osmatch.name
+$liveHosts | ForEach-Object {
+    $entry = New-Object psobject
+    if ($_.address.addr.count -gt 1){
+        $entry | Add-Member -MemberType NoteProperty -Name "IP" -Value $_.address.addr[0]
+        $entry | Add-Member -MemberType NoteProperty -Name "MAC" -Value $_.address.addr[1]
+    } 
+    else {
+        $entry | Add-Member -MemberType NoteProperty -Name "IP" -Value $_.address.addr
+    }
+    if ($_.os.osmatch.name){
+        $entry | Add-Member -MemberType NoteProperty -Name "OS" -Value $_.os.osmatch.name
+    }
 
-$sesh[0].psobject.Properties | ? {$_.typenameofvalue -notlike 'system.string'}
+    $targets += $entry
+}
+
+
+# $sesh[0].psobject.Prerties | ? {$_.typenameofvalue -notlike 'system.string'}
